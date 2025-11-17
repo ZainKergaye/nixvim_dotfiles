3b92a0909827360cb96fd94a7f786ac876a6a560
Zain Kergaye <zain4utah@gmail.com>
2025-11-17 11:13:43 -0700

    Changed c lsp version

--- languages/c.nix
+++ languages/c.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }: {
+{ system, pkgs, ... }:
+{
   plugins = {
-    lsp.servers.clangd = {
-      enable = true;
-    };
+    lsp.servers.clangd =
+      let
+        cpkgs =
+          # cland-tools version 11
+          import
+            (builtins.fetchGit {
+              name = "old-2019-pkgs";
+              url = "https://github.com/NixOS/nixpkgs/";
+              ref = "refs/heads/nixpkgs-unstable";
+              rev = "5c1ffb7a9fc96f2d64ed3523c2bdd379bdb7b471";
+            })
+            { inherit system; };
+
+        myPkg = cpkgs.clang-tools;
+      in
+      {
+        enable = true;
+        package = myPkg;
+      };
     none-ls.sources.formatting.clang_format.enable = true;
 
     dap-lldb = {
